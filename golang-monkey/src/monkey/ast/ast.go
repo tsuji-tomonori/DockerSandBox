ed6361ad5394b860266babca6c94292b11485888
tsuji-tomonori <160441107@ccalumni.meijo-u.ac.jp>
2022-09-21 17:11:58 +0900

    let構文解析

---
+++ golang-monkey/src/monkey/ast/ast.go
@@ -0,0 +1,61 @@
+package ast
+
+import "monkey/token"
+
+type Node interface {
+	TokenLiteral() string
+}
+
+// 文: 値を生成しない
+type Statement interface {
+	// 文: 値を生成しない
+	Node
+	statementNode()
+}
+
+type Expression interface {
+	// 式: 値を生成する
+	Node
+	expressionNode()
+}
+
+type Program struct {
+	// root ノード
+	Statement []Statement
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statement) > 0 {
+		return p.Statement[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
+
+type LetStatement struct {
+	Token token.Token // token.LET トークン
+	Name  *Identifier // 識別のため
+	Value Expression  // let文の中で値を生成する式のため
+}
+
+func (ls *LetStatement) statementNode() {
+
+}
+
+func (ls *LetStatement) TokenLiteral() string {
+	return ls.Token.Literal
+}
+
+type Identifier struct {
+	// let x = 5;におけるxのような束縛の識別子を保持する
+	Token token.Token // token.IDENT トークン
+	Value string
+}
+
+func (i *Identifier) expressionNode() {
+	// ノードの種類を少なく保つため変数束縛の名前を表現するために式として扱う
+}
+
+func (i *Identifier) TokenLiteral() string {
+	return i.Token.Literal
+}
---
+++ golang-monkey/src/monkey/parser/parser.go
@@ -0,0 +1,91 @@
+package parser
+
+import (
+	"monkey/ast"
+	"monkey/lexer"
+	"monkey/token"
+)
+
+type Parser struct {
+	l *lexer.Lexer
+
+	curToken  token.Token // 現在
+	peekToken token.Token // 次
+}
+
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{l: l}
+	// 2つのトークンを読み込む
+	// curTokenとpeekTokenの両方がセットされる
+	p.nextToken()
+	p.nextToken()
+
+	return p
+}
+
+func (p *Parser) nextToken() {
+	p.curToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
+
+func (p *Parser) ParserProgram() *ast.Program {
+	program := &ast.Program{}
+	program.Statement = []ast.Statement{}
+
+	for p.curToken.Type != token.EOF {
+		stmt := p.parseStatement()
+		if stmt != nil {
+			program.Statement = append(program.Statement, stmt)
+		}
+		p.nextToken()
+	}
+
+	return program
+}
+
+func (p *Parser) parseStatement() ast.Statement {
+	switch p.curToken.Type {
+	case token.LET:
+		return p.paseLetStatement()
+	default:
+		return nil
+	}
+}
+
+func (p *Parser) paseLetStatement() *ast.LetStatement {
+	stmt := &ast.LetStatement{Token: p.curToken}
+
+	if !p.expectPeek(token.IDENT) {
+		return nil
+	}
+
+	stmt.Name = &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
+
+	if !p.expectPeek(token.ASSIGN) {
+		return nil
+	}
+
+	// TODO: セミコロンに遭遇するまで式を読み飛ばしてしまっている
+	for !p.curTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+
+	return stmt
+}
+
+func (p *Parser) curTokenIs(t token.TokenType) bool {
+	return p.curToken.Type == t
+}
+
+func (p *Parser) peekTokenIs(t token.TokenType) bool {
+	return p.peekToken.Type == t
+}
+
+func (p *Parser) expectPeek(t token.TokenType) bool {
+	if p.peekTokenIs(t) {
+		p.nextToken()
+		return true
+	} else {
+		return false
+	}
+}
